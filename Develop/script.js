f78641cea94f549f82bccc43d80f6ee05269ca0b
Ivy Bean <ivieking@gmail.com>
2020-07-10 16:18:37 -0400

    JavaScript- added criteria prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Length of password
+var charAmount = prompt("Enter the amount of charcters needed for your password. (minimum of 8/maximum of 128)");
+console.log(charAmount);
+
+// lower/upper/special characters
+var lowerCase = confirm("Should your password contain lowercase letters?");
+console.log(lowerCase);
+
+var upperCase = confirm("Should your password contain uppercase letters?");
+console.log(upperCase);
+
+var specialChar = confirm("Should your password contain special characters?");
+console.log(specialChar);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
